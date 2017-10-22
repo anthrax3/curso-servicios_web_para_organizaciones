7f08315c6bd6cd4b02f52b83284ba0784f88f546
Jorge Díaz <jorge.diaz@gmail.com>
2017-10-21 21:07:09 -0500

    Update 00-servidor-principal_preparativos-01.sh

--- 00-servidor-principal_preparativos-01.sh
+++ 00-servidor-principal_preparativos-01.sh
@@ -20,7 +20,7 @@ echo -ne "\n# Integra Conocimiento e Innovación - http://integraci.com.mx      
 echo -ne "\n#                                     contacto@integraci.com.mx               #"
 echo -ne "\n###############################################################################\n\n"
 
-# Detección de usuario, se debe ejecutar como usuario root de lo contrario
+# Detección de usuario, se debe ejecutar como usuario root (Usuario "0") de lo contrario
 # termina la ejecución.
 if [[ $EUID -ne 0 ]]; then
     echo -ne "\nPor favor ejecuta el script como usuario \"root\".\n"
@@ -29,26 +29,49 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# En caso de existir el archvio "os-release"
 if [ -e /etc/os-release ]; then
 
   # Lee el archivo con las variables de identificación de la distribución.
   # $ID, $VERSION_CODENAME, $VERSION_ID y $PRETTY_NAME
   source /etc/os-release
-  dist=$ID
+  
+  # Asigna el valor de la variable ID del sistema obtenida mediante el comando source, 
+  # a la variable del "DISTRIBUCION" script
+  DISTRIBUCION=$ID
 
   echo -ne "Distribución: $PRETTY_NAME\n\n"
 
-  # Ejecuta los comandos de acuerdo a la distribución identificada
-  case "${dist}" in
+  # Compara el valor de la variable DISTRIBUCION y ejecuta los comandos de acuerdo a 
+  # la distribución.
+  case "${DISTRIBUCION}" in
     fedora)
+      # Instalamos Git
       dnf -y install git
+
+      # Instalamos Ansible
       dnf -y install ansible
+      
+      # Instalamos OpenSSH
       dnf -y install openssh-server
+      
+      # Iniciamos el servicio de SSH
       systemctl start sshd.service
+      
+      # Hacemos que se ejecute SSH al arrancar el equipo
       systemctl enable sshd.service
+      
+      # Añadimos el servicio de SSH a las reglas del Firewall
+      firewall-cmd --add-service=ssh --permanent
+      
+      # Recargamos las reglas del Firewall
+      firewall-cmd --reload
       ;;
     centos)
+      # Instalamos Git
       yum -y install git
+      
+      # Instalamos Ansible
       yum -y install ansible
       ;;
     *)
